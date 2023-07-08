127f8e43d3fbf3cc71e7e864cc5c184c9fa09e15
Nguyễn Kiêm Quang <133070247+quangnkhe171298@users.noreply.github.com>
2023-07-08 17:39:02 +0700

    create logout.java

---
+++ webNews/src/java/controller/logout.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author kiemq
+ */
+public class logout extends HttpServlet {
+
+     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        HttpSession session = req.getSession();
+        session.removeAttribute("account");
+        req.getRequestDispatcher("newslist").forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        HttpSession session = req.getSession();
+        session.removeAttribute("account");
+        req.getRequestDispatcher("newslist").forward(req, resp);
+    }
+
+}
