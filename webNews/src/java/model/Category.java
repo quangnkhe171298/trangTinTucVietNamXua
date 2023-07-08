6848529eeea2e7e90fc77448bcccec6cb5fee6ce
Nguyễn Kiêm Quang <133070247+quangnkhe171298@users.noreply.github.com>
2023-07-08 17:41:56 +0700

    update category.java

--- webNews/src/java/model/Category.java
+++ webNews/src/java/model/Category.java
@@ -11,6 +11,7 @@ package model;
 public class Category {
     private int id;
     private String name;
+    private String image;
 
     public Category() {
     }
@@ -18,6 +19,25 @@ public class Category {
     public Category(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Category(int id, String name, String image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public Category(String name, String image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 
     public Category(String name) {
