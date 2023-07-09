c677a077b824ff224b1131254d702b03129b2817
Nguyễn Kiêm Quang <133070247+quangnkhe171298@users.noreply.github.com>
2023-07-10 00:48:57 +0700

    create DeleteNews.java

---
+++ webNews/src/java/controller/DeleteNews.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dal.NewsDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kiemq
+ */
+public class DeleteNews extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        NewsDao dao = new NewsDao();
+        dao.deleteNews(id);
+        resp.sendRedirect("admin");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        NewsDao dao = new NewsDao();
+        dao.deleteNews(id);
+        resp.sendRedirect("admin");
+    }
+
+}
